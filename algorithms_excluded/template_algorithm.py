3dab2c7ff6caf136378c1f13d70bd75baacc4861
Ultra Sheep <sheepdesnay@gmail.com>
2024-11-13 00:27:03 +0800

    updated template

--- algorithms_excluded/template_algorithm.py
+++ algorithms_excluded/template_algorithm.py
@@ -1,6 +1,8 @@
 import numpy as np
 from evaluate_population import evaluate_population
 
+# other parameters
+
 class algorithm:    
     def __init__(self, pop_size, tournament_size, generations, benchmark):
         self.name = "example algorithm"
@@ -13,10 +15,14 @@ class algorithm:
         population = initial_population
         best_fitness_history = []
         best_fitness = float('inf')
-        
-        # algorithm here
-        #fitness_values = evaluate_population(population, self.benchmark)
+        for generation in range(self.generations):
 
+            # algorithm here
+
+            fitness_values = evaluate_population(population, self.benchmark)
+            generation_best = min(fitness_values)
+            if generation_best < best_fitness:
+                best_fitness = generation_best
+            best_fitness_history.append(best_fitness)
 
         return best_fitness, best_fitness_history
-
