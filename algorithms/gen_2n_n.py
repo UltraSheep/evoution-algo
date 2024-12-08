6913ccce1fd7ac42c7b08a84af4342b11b36f9ee
max52015 <choyihun52015@gmail.com>
2024-12-09 02:52:35 +0800

    feat: gen_2n_n.py rewite to be called by godot

--- algorithms/gen_2n_n.py
+++ algorithms/gen_2n_n.py
@@ -1,86 +1,65 @@
 import numpy as np
-from .. import config
-from ..utils.evaluate_population import evaluate_population
+import config
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 c_rate = 0.80
 m_rate = 0.20
 
-class algorithm:    
+pre_fitness = None  # Declare fitness as a global variable
+init_pop_fitness = None
+
+class INDIVIDUAL(BaseModel):
+    id: int
+    health: int
+    weapon: int
+    speed: int
+    jump: int
+
+class POPULATION(BaseModel):
+    pop: List[INDIVIDUAL]
+
+class RESULT(BaseModel):
+    fitness: List[int]
+
+class Algorithm:    
     def __init__(self):
         self.name = "gen_2n_n algorithm"
         self.pop_size = config.POP_SIZE
         self.generations = config.GENERATIONS
         self.benchmark = config.BENCHMARK.function
         self.tournament_size = config.TOURNAMENT_SIZE
-        self.params= config.BENCHMARK
+        self.params = config.BENCHMARK
         self.rs = np.random.RandomState(config.SEED)
 
-    def train(self, initial_population):
-        population = self.strip_sigma(initial_population)
-        best_fitness_history = []
-        best_fitness = float('inf')
-        
-        for generation in range(self.generations):
-            global c_rate
-            c_rate = 0.8 * (1 - generation / self.generations) + 0.4 * (generation / self.generations)
-            global m_rate
-            m_rate = 0.2 * (1 - generation / self.generations) + 0.01 * (generation / self.generations)
-
-            population , best_fitness = self.evolve_c_2n_n (population)
-
-            # 5 generations without improvement
-            # 還沒用
-            # best_fitness_streak = 0
-            # previous_best_fitness = float('inf')
-            # if abs(best_fitness - previous_best_fitness) < 1e-6:
-            #     best_fitness_streak = best_fitness_streak + 1
-            # else:
-            #     best_fitness_streak = 0
-            # previous_best_fitness = best_fitness
-
-            # if best_fitness_streak >= 5:
-            #     m_rate = min(m_rate * 1.5 , 0.5)
-            #     c_rate = 0.9
-
-            best_fitness_history.append (best_fitness)
-
-        return best_fitness , best_fitness_history
-            
-    def strip_element_sigma(self, element):
-        x, _ = element
+    def strip_element_sigma (self , element):
+        x , _ = element
         return x
     
-    def strip_sigma(self, population):
-        return [self.strip_element_sigma (ind) for ind in population]
+    def strip_sigma (self , population):
+        return [self.strip_element_sigma(ind) for ind in population]
 
     def elite_selection (self , pop , fitness):
         elite_count = max(1 , int(self.pop_size * 0.1))
         sorted_indices = np.argsort(fitness)
         elite_individuals = [pop[i] for i in sorted_indices[:elite_count]]
         elite_fitness = [fitness[i] for i in sorted_indices[:elite_count]]
-        return elite_individuals, elite_fitness[0]
+        return elite_individuals , elite_fitness[0]
 
     def tournament_selection (self , n , pop , fitness):
         selected = []
-        for i in range (n):
-            # 2 pick 1
-            # idx1 , idx2 = self.rs.choice (range(len(pop)), 2, replace = False)
-            # if fitness[idx1] < fitness[idx2]:
-            #     selected.append(pop[idx1])
-            # else:
-            #     selected.append(pop[idx2])
-
-            # tournament_size pick 1
-            candidates = self.rs.choice(range(len(pop)) , self.tournament_size , replace = False)
-            best_idx = min (candidates , key = lambda idx: fitness[idx])
-            selected.append(pop[best_idx])
+        for i in range(n):
+            candidates = self.rs.choice (range(len(pop)) , self.tournament_size , replace = False)
+            best_idx = min(candidates , key = lambda idx: fitness[idx])
+            selected.append (pop[best_idx])
         return selected
 
     def simulated_binary_crossover (self , parent1 , parent2 , eta = 0.5):
-        child1 , child2 = np.copy(parent1) , np.copy(parent2)
-        for i in range (len (parent1)):
-            if self.rs.uniform(0 , 1) <= c_rate:
-                u = self.rs.uniform(0 , 1)
+        child1 , child2 = np.copy (parent1) , np.copy (parent2)
+        for i in range (len(parent1)):
+            if self.rs.uniform (0 , 1) <= c_rate:
+                u = self.rs.uniform (0 , 1)
                 if u <= 0.5:
                     beta = (2 * u) ** (1 / (eta + 1))
                 else:
@@ -89,104 +68,57 @@ class algorithm:
                 child1[i] = 0.5 * ((1 + beta) * parent1[i] + (1 - beta) * parent2[i])
                 child2[i] = 0.5 * ((1 - beta) * parent1[i] + (1 + beta) * parent2[i])
         
-            child1[i] = np.clip(child1[i], self.params.SMin , self.params.SMax)
-            child2[i] = np.clip(child2[i], self.params.SMin , self.params.SMax)
+            child1[i] = np.clip(child1[i] , self.params.SMin, self.params.SMax)
+            child2[i] = np.clip(child2[i] , self.params.SMin, self.params.SMax)
         return child1 , child2
 
     def mutate (self , individual):
-        for i in range (len (individual)):
-            # print (m_rate)
+        for i in range (len(individual)):
             if self.rs.uniform (0 , 1) <= m_rate:
                 individual[i] += self.rs.uniform (-1 , 1) * (self.params.SMax - self.params.SMin) * 0.1
                 individual[i] = np.clip (individual[i] , self.params.SMin , self.params.SMax)
         return individual
-    
-    def evolve_elite (self , pop):
-        fitness = evaluate_population(pop , self.benchmark)
-        best_individuals , best_fitness = self.elite_selection (pop , fitness)
 
-        new_population = best_individuals
-        parents = self.tournament_selection (self.pop_size - 1 , pop , fitness)
-        
-        # Random selection 10% of the population
-        # random_count = max(1, int(self.pop_size * 0.1))
-        # random_indices = self.rs.choice(range(len(pop)) , random_count , replace=False)
-        # random_individuals = [pop[i] for i in random_indices]
-        # new_population = best_individuals + random_individuals
-        # parents = self.tournament_selection(self.pop_size - len(new_population) , pop , fitness)
-
-        for i in range (0 , len (parents) , 2):
-            parent1 , parent2 = parents[i] , parents[min (i + 1 , len (parents) - 1)]
-            child1 , child2 = self.simulated_binary_crossover (parent1 , parent2)
-            new_population.append (self.mutate (child1))
-            if len (new_population) < self.pop_size:
-                new_population.append (self.mutate (child2))
-
-        return new_population, best_fitness
-
-    def evolve_c_2n_n (self , pop):
-        fitness = evaluate_population(pop , self.benchmark)
-        parents = self.tournament_selection (self.pop_size , pop , fitness)
+    def evolve(self , pop , parents):
         offspring = []
-        for i in range (0, len(pop), 2):
-            # parent1 , parent2 = pop[i] , pop[i + 1]
-            # parent1 = pop[i]
-            # parent2 = pop[min(i + 1, len(pop) - 1)]
-            parent1 , parent2 = parents[i] , parents[min (i + 1 , len (parents) - 1)]
+        for i in range (0 , len(parents) , 2):
+            parent1 , parent2 = parents[i] , parents[min(i + 1 , len(parents) - 1)]
             child1 , child2 = self.simulated_binary_crossover (parent1 , parent2)
             offspring.append (self.mutate(child1))
             offspring.append (self.mutate(child2))
 
-        combined_population = parents + offspring
-        combined_fitness = evaluate_population (combined_population , self.benchmark)
+        combined_population = pop + offspring
+        return combined_population , offspring
+    
+    def evolve_c_2n_n (self , pop , curr_fitness):
+        global pre_fitness
+        offspring = []
+
+        if pre_fitness is None:
+            combined_population , offspring = self.evolve (pop , pop)
+            pre_fitness = curr_fitness
+            return offspring , curr_fitness[0]
+
+        parents = self.tournament_selection (self.pop_size , pop , pre_fitness)
+
+        combined_population , _ = self.evolve (pop , parents)
+        combined_fitness = pre_fitness + curr_fitness
         sorted_indices = np.argsort (combined_fitness)
         new_population = [combined_population[i] for i in sorted_indices[:self.pop_size]]
-        best_fitness = combined_fitness[sorted_indices[0]]
+        
+        # update global fitness
+        pre_fitness = [combined_fitness[i] for i in sorted_indices[:self.pop_size]]
 
-        return new_population , best_fitness
+        return new_population , pre_fitness[0]
 
-    def evolve_lsgop (self , pop):
-            # tournament selection with size 5% of population
-            tournament_size = max(1 , int(self.pop_size * 0.05))
-            parents = []
-            for _ in range (self.pop_size):
-                candidates_1 = self.rs.choice (range (len(pop)) , tournament_size, replace = False)
-                parent1 = min (candidates_1 , key = lambda idx: evaluate_population ([pop[idx]] , self.benchmark)[0])
-                candidates_2 = self.rs.choice (range (len(pop)) , tournament_size, replace = False)
-                parent2 = min (candidates_2 , key = lambda idx: evaluate_population ([pop[idx]] , self.benchmark)[0])
-                parents.append ((pop[parent1] , pop[parent2]))
+algo = Algorithm()
 
-            offspring = []
+def train (population_data , fitness_data):
+    population = [[ind.health , ind.weapon , ind.speed , ind.jump] for ind in population_data]
+    new_population_data , _ = algo.evolve_c_2n_n (population , fitness_data)
 
-            # crossover  generate self.pop_size
-            for parent1 , parent2 in parents:
-                child = np.zeros_like (parent1)
-                for i in range (len(parent1)):
-                    fitness_values = []
-                    candidates = [parent1[i] , parent2[i] , (parent1[i] + parent2[i]) / 2]
-                    for value in candidates:
-                        child[i] = value
-                        fitness_values.append (evaluate_population([child] , self.benchmark)[0])
-                    best_value = candidates [np.argmin (fitness_values)]
-                    child[i] = best_value
-                offspring.append(child)
+    new_population = []
+    for i , data in enumerate (new_population_data):
+        new_population.append (INDIVIDUAL (id = i , health = data[0] , weapon = data[1] , speed = data[2] , jump = data[3]))
 
-            # mutation all
-            for child in offspring:
-                for i in range (len(child)):
-                    epsilon = self.rs.uniform (-1 / (2 * self.generations) , 1 / (2 * self.generations))
-                    candidates = [child[i] , child[i] + epsilon , child[i] - epsilon]
-                    fitness_values = [evaluate_population([child] , self.benchmark)[0] for _ in candidates]
-                    best_value = candidates[np.argmin(fitness_values)]
-                    child[i] = best_value
-
-            # selection self.pop_size 
-            combined_population = pop + offspring
-            combined_fitness = evaluate_population(combined_population , self.benchmark)
-            sorted_indices = np.argsort(combined_fitness)
-            new_population = [combined_population[i] for i in sorted_indices[:self.pop_size]]
-
-            while len(new_population) < self.pop_size:
-                new_population.append(self.rs.uniform(self.params.SMin , self.params.SMax , len(pop[0])))
-
-            return new_population, combined_fitness[sorted_indices[0]]
+    return new_population
