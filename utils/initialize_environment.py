b8daf13eb2ed1c2aa272a226b3d4d77cea38efa6
Ultra Sheep <sheepdesnay@gmail.com>
2024-12-18 02:36:57 +0800

    switch to json for logs

--- utils/initialize_environment.py
+++ utils/initialize_environment.py
@@ -1,11 +1,21 @@
 import os
 import datetime
+import json
 from pathlib import Path
 from .. import config
 
 def initialize_environment():
     Path("./results").mkdir(exist_ok=True)
-    with open(f"./results/{config.RESULTS_FILE}_{datetime.datetime.now().strftime('%Y_%m_%d_%H%M%S')}.txt", "w") as file:
-        os.environ["result_file"]=file.name
-        file.write(f"Population size = {config.POP_SIZE}\nTournament size = {config.TOURNAMENT_SIZE}\nEnemy per level = {config.ENEMY_COUNT}\n")
-        file.close()
+    
+    file_name = f"./results/{config.RESULTS_FILE}_{datetime.datetime.now().strftime('%Y_%m_%d_%H%M%S')}.json"
+    
+    initial_data = {
+        "population_size": config.POP_SIZE,
+        "tournament_size": config.TOURNAMENT_SIZE,
+        "enemies_per_level": config.ENEMY_COUNT,
+        "generations": []
+    }
+    
+    with open(file_name, "w") as file:
+        os.environ["result_file"] = file_name
+        json.dump(initial_data, file, indent=4)
--- utils/log_results.py
+++ utils/log_results.py
@@ -1,11 +1,38 @@
 import os
+import json
 from .structures import *
 
 def log(generation, population: POPULATION, result: RESULT):
-    with open(os.environ['result_file'], "a") as file:
-        file.write(f"Gen {generation} :\n")
-        for i, level in enumerate(population.pop):
-            file.write(f"    enemies:\n")
-            for enemy in level.enemies:
-                file.write(f"        {enemy}\n")
-            file.write(f"    score: {result.fitness[i]}\n\n")
+    result_file = os.environ.get("result_file")
+
+    generation_data = {
+        "generation_id": generation,
+        "groups": []
+    }
+
+    for i, level in enumerate(population.pop):
+        group_data = {
+            "group_id": i,
+            "enemies": [],
+            "score": result.fitness[i]
+        }
+        
+        for enemy in level.enemies:
+            enemy_data = {
+                "id": enemy.id,
+                "health": enemy.health,
+                "weapon": enemy.weapon,
+                "speed": enemy.speed,
+                "jump": enemy.jump
+            }
+            group_data["enemies"].append(enemy_data)
+        
+        generation_data["groups"].append(group_data)
+
+    with open(result_file, "r") as file:
+        all_data = json.load(file)
+    
+    all_data["generations"].append(generation_data)
+    
+    with open(result_file, "w") as file:
+        json.dump(all_data, file, indent=4)
